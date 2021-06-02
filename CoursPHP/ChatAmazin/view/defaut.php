690e00e1158244a6c17294041f8c33d4afcd2d34
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-02 11:52:05 +0200

    update ChatAmazin

---
+++ CoursPHP/ChatAmazin/view/defaut.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Amazin</title>
+    <link rel="styleshhet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
+</head>
+
+<body>
+    <?PHP
+    // require('view/header.php');
+    require('view/chat.php');
+    require('view/form.php');
+    //footer1
+    //footer2
+    ?>
+
+</body>
+
+</html>
