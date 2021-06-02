1fbbab0f60f1a3cf3764bcf3d0ae2e98053609b9
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-02 08:31:44 +0200

    Training exercises (ongoing)

---
+++ CoursPHP/Exercices/exercice.php
@@ -0,0 +1,61 @@
+<?php
+
+
+// Exercice 1 : afficher le plus petit nombre
+$a = 10;
+$b = 20;
+
+// Ecrire le code ici
+
+echo "<hr>";
+
+// Exercice 2 : afficher la table de 9 (opération + résultat)
+
+// Ecrire le code ici
+
+echo "<hr>";
+
+// Exercice 3 : compter à rebours
+
+// Ecrire le code ici
+
+echo "<hr>";
+
+// Exercice 4 : Ecrire une boucle qui produit une ligne horizontale de 8 étoiles
+
+// Ecrire le code ici
+
+echo "<hr>";
+
+// Exercice 5 : Afficher un figure de 8 étoiles sur 8.
+
+// Ecrire le code ici
+
+echo "<hr>";
+
+// Exercice 6 : Ecrire une fonction (utilisant une boucle) qui remplace tout les espaces d'une phrase par un underscore
+// indice : si $mot = "jouet" alors $mot[0] = "j", $mot[1] = "o", ... 
+// indice : chercher sur google une fonction qui renvoie la longueur d'une chaine de caractères
+
+// Ecrire le code ici
+
+// trouver sur google son équivalant clef en main.
+echo "<hr>";
+
+// Exercice 7 : Ecrire une fonction (utilisant une boucle) qui inverse et affiche l'ordre des lettres d'un mot
+
+// Ecrire le code ici
+
+// trouver sur google son équivalant clef en main.
+echo "<hr>";
+
+
+
+
+// Exercice 8: Ecrire une fonction qui supprime les espaces et met la phrase en camelCase
+// Interdit d'utiliser les fonctions suivantes : ucwords() et str_replace().
+$sentence = "le chat est mort"; //devient : leChatEstMort
+
+// Ecrire le code ici
+
+// Réécrire la fonction en utilisant les fonctions interdites
