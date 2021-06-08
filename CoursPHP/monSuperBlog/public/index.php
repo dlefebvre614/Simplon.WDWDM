5eb18643f203ed597209090f41d66426cb830172
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-08 04:52:56 +0200

    Move index.php to public

---
+++ CoursPHP/monSuperBlog/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$path = dirname(__DIR__);
+//var_dump($path);
+//die();
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'post.home';
+
+ob_start();
+try {
+    if ($page === 'post.home') {
+
+        require $path . '/model/postRepository.php';
+        $posts = findALL();
+
+        require $path . '/view/post/home.php';
+    } elseif ($page === 'post.show') {
+
+        require $path . '/model/postRepository.php';
+        $post = findOneById($_GET['id']);
+
+        require $path . '/view/post/show.php';
+    } elseif ($page === 'user.connect') {
+        require $path . '/view/user/connectionForm.php';
+    } else {
+        throw new Exception('404');
+    }
+} catch (Exception $e) {
+    require $path . '/view/error/error404.php';
+}
+$content = ob_get_clean();
+
+require $path . '/view/base.php';
