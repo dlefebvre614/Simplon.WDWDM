9976de1a4b2793f891d579c59c18b0c61795f883
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-09 06:54:51 +0200

    Add render

--- CoursPHP/monSuperBlog/controller/postController.php
+++ CoursPHP/monSuperBlog/controller/postController.php
@@ -2,26 +2,41 @@
 
 // $path = dirname(__DIR__);
 
-function home()
+function render($view, $datas)
 {
     $path = dirname(__DIR__);
+
+    extract($datas);
+
     ob_start();
-    require $path . '/model/postRepository.php';
-    $posts = findALL();
+    require $path .  '/view/post/' . $view . '.php';
     $content = ob_get_clean();
 
     require $path . '/view/base.php';
-    require $path . '/view/post/home.php';
+}
+
+function home()
+{
+    $path = dirname(__DIR__);
+    //ob_start();
+    require $path . '/model/postRepository.php';
+    $posts = findALL();
+    //$content = ob_get_clean();
+
+    //require $path . '/view/base.php';
+    //require $path . '/view/post/home.php';
+    render('home', compact('posts'));
 }
 
 function show()
 {
     $path = dirname(__DIR__);
-    ob_start();
+    //ob_start();
     require $path . '/model/postRepository.php';
     $post = findOneById($_GET['id']);
-    $content = ob_get_clean();
+    //$content = ob_get_clean();
 
-    require $path . '/view/base.php';
-    require $path . '/view/post/show.php';
+    //require $path . '/view/base.php';
+    //require $path . '/view/post/show.php';
+    render('show', compact('post'));
 }
